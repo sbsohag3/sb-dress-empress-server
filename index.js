6d968915c775ab6b266019c6a615836d0562c415
Md.Shohagh <96836985+sbsohag3@users.noreply.github.com>
2022-05-05 11:54:29 +0600

    update section done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const { get } = require("express/lib/response");
 const port = process.env.PORT || 5000;
 const app = express();
@@ -15,14 +15,13 @@ const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
-  
 });
 
 // http://localhost:5000/
 async function run() {
   try {
     await client.connect();
-    const productsCollection = client.db("sbDress").collection('products');
+    const productsCollection = client.db("sbDress").collection("products");
 
     // http://localhost:5000/products
     app.get("/products", async (req, res) => {
@@ -31,17 +30,33 @@ async function run() {
       const products = await cursor.toArray();
       res.send(products);
     });
-
-    //Crreate Post
-    // http://localhost:5000/login
-
-    app.post('/login', async(req, res)=>{
-      const data = req.body;
-      console.log(data)
-      const result = await productsCollection.insertOne(data)
-      res.send(result)
-    })
     //Update Post
+    app.put("/product/:id", async(req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      console.log("from dat update", data);
+
+      // console.log('from put', id)
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+
+      const updateDoc = {
+        $set: {
+          stock: data.stock
+        },
+      };
+       const result = await productsCollection.updateOne(filter, updateDoc, options);
+       res.send(result);
+    });
+
+    //Create Post
+    // http://localhost:5000/product
+
+    app.post("/product", async (req, res) => {
+      const data = req.body;
+      const result = await productsCollection.insertOne(data);
+      res.send(result);
+    });
   } finally {
   }
 }
