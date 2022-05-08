547a659ba3fcd591cf37316539c35305461aeee8
Md.Shohagh <96836985+sbsohag3@users.noreply.github.com>
2022-05-08 09:16:07 +0600

    all done

--- index.js
+++ index.js
@@ -11,22 +11,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// function verifyJWT(req, res, next) {
-//   const authHeader = req.headers.authorization;
-//   if (!authHeader) {
-//     return res.status(401).send({ message: "unauthorized access" });
-//   }
-//   const token = authHeader.split(" ")[1];
-//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-//     if (err) {
-//       return res.status(403).send({ message: "Forbidden access" });
-//     }
-//     console.log("decoded", decoded);
-//     req.decoded = decoded;
-//   });
-//   next();
-// }
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@sbsohag.2i4yv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -39,7 +23,6 @@ async function run() {
   try {
     await client.connect();
     const productsCollection = client.db("sbDress").collection("products");
-    const uploadProductCollection = client.db("sbDress").collection("myItems");
 
     //Auth
     app.post("/login", async (req, res) => {
@@ -77,7 +60,6 @@ async function run() {
 
       const updateDoc = {
         $set: {
-          // price: data.price,
           stock: data.stock,
         },
       };
